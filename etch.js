f1e77ba0275feb4a1272eaf1ebc59d970e02f93c
Amanda C. <219941056+manda-2016@users.noreply.github.com>
2025-11-30 19:55:17 -0500

    Button added Prompt added

--- etch.js
+++ etch.js
@@ -1,7 +1,7 @@
 //creates divs
 for (i = 0 ; i < 256; i++){
     const squareDiv = document.createElement ("div");
-    const newContent =document.createTextNode("Hello");
+    const newContent = document.createTextNode("");
     squareDiv.appendChild(newContent);
     squareDiv.id = "boxes";
     const element = document.getElementById("boxContainer");
@@ -11,13 +11,37 @@ for (i = 0 ; i < 256; i++){
 
     squareDiv.addEventListener ("mouseenter", (event) => {
     event.target.style.backgroundColor = "orange";
-    setTimeout (() => {
-        event.target.style.backgroundColor = "";
-
-    }, 500);
+    //setTimeout (() => {
+        //event.target.style.backgroundColor = "";
+    //}, 5000);
 })};
 
+//creates button to change grid size
+function popupButton () {
+    console.log("hello");
+    const popButton = document.createElement ("BUTTON");
+    const text = document.createTextNode ("Change Grid Size");
+    popButton.appendChild(text);
+    const element = document.getElementById("promptButton");
+    element.appendChild(popButton);
 
+
+    popButton.addEventListener("click", (event) => {
+        let userInput = prompt ("Please enter a number between 1 and 100 to change the grid size");
+    });
+    //let i = 0;
+    //let i = userInput;
+    
+
+    //if (userInput > 100 || userInput < 1) {
+        //alert ("Invalid entry. Please enter a number between 1 and 100")
+    //} else {
+
+    //}
+
+};
+
+popupButton()
 
 //const title =document.createElement("h1");
 //title.id= "title";
